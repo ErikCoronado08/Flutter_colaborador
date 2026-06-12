638418e46dbdd1301d6bcfbe0fd927cb8219f5f7
roxanagutierrez393 <roxanagutierrez393@gmail.com>
2026-06-11 21:37:32 -0600

    feat: integracion y configuracion del ecosistema de 8 plugins en pubspec.yaml
    - Correccion de 'SlidableAction' adaptada a flutter_slidable v3.1.2 (foregroundColor)
    - Implementacion de alerta sonora con audioplayers v6.0.0 y asset ping.mp3
    - Remocion de etiquetas 'Free' en SolicitudesScreen para unificación oficial de JobHub
    - Preparacion de dependencias para Mapa (flutter_map, geolocator, latlong2, polylines) y Chat

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
 import desktop_drop
 import file_picker
 import file_selector_macos
 import flutter_local_notifications
+import geolocator_apple
 import path_provider_foundation
 import printing
 import share_plus
@@ -17,10 +19,12 @@ import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
